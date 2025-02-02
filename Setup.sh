dc0d1f02eb5c60ef0116fa445c6119584a169e27
Ryan Van Massenhoven <ryanvanmassenhoven@gmail.com>
2025-02-02 17:32:14 -0500

    Move update prompt to be second last

--- Setup.sh
+++ Setup.sh
@@ -187,21 +187,6 @@ EOF
     esac
   fi
   
-  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "UPDATE" --menu "\nUpdate Proxmox VE now?" 11 58 2 \
-    "yes" " " \
-    "no" " " 3>&2 2>&1 1>&3)
-  case $CHOICE in
-  yes)
-    msg_info "Updating Proxmox VE (Patience)"
-    apt-get update &>/dev/null
-    apt-get -y dist-upgrade &>/dev/null
-    msg_ok "Updated Proxmox VE"
-    ;;
-  no)
-    msg_error "Selected no to Updating Proxmox VE"
-    ;;
-  esac
-
   CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "Storage" --menu "\nConfigure Fstab for the following Network Shares?\n/mnt/pve/ISO\mnt\pve/VM-Storage" 11 58 2 \
     "yes" " " \
     "no" " " 3>&2 2>&1 1>&3)
@@ -217,6 +202,21 @@ EOF
   no)
     msg_error "Selected no to Rebooting Proxmox VE (Reboot recommended)"
     msg_ok "Completed Post Install Routines"
+    ;;
+  esac
+
+  CHOICE=$(whiptail --backtitle "Proxmox VE Helper Scripts" --title "UPDATE" --menu "\nUpdate Proxmox VE now?" 11 58 2 \
+    "yes" " " \
+    "no" " " 3>&2 2>&1 1>&3)
+  case $CHOICE in
+  yes)
+    msg_info "Updating Proxmox VE (Patience)"
+    apt-get update &>/dev/null
+    apt-get -y dist-upgrade &>/dev/null
+    msg_ok "Updated Proxmox VE"
+    ;;
+  no)
+    msg_error "Selected no to Updating Proxmox VE"
     ;;
   esac
 
